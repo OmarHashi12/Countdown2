15834fab4917de3d9cdd7093b49eb3db438ff3fe
Omar <hmj9gf@Virginia.edu>
2024-05-17 13:49:50 -0400

    new changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TriviaGame from './components/TriviaGame';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Trivia Game</h1>
+      </header>
+      <TriviaGame />
+    </div>
+  );
+}
+
+export default App;
